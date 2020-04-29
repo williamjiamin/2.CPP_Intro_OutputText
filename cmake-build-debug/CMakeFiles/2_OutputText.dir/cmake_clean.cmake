18dcc62881e4452944f1436c1553196c4e445b75
williamjiamin <lxodgf@163.com>
2020-04-29 14:06:10 +0800

    init commit

---
+++ cmake-build-debug/CMakeFiles/2_OutputText.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "2_OutputText"
+  "2_OutputText.pdb"
+  "CMakeFiles/2_OutputText.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2_OutputText.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Welcome to our course, loading.........OK!" << flush;
+    cout << "a" << " " << "b" << " " << "c" << endl;
+
+    cout << "Welcome to LXOD's CPP course , I am William" << endl;
+    cout << "Welcome to LXOD's C course , I am William" << endl;
+    cout << "Welcome to LXOD's Java course , I am William" << endl;
+    cout << "Welcome to LXOD's Python course , I am William" << endl;
+    cout << "Welcome to LXOD's Fintech course , I am William" << endl;
+    cout << "Welcome to LXOD's Quant course , I am William" << endl;
+}
